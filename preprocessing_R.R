244daf97175cff49b89eb16cbb8f56580ffb3558
ie017 <elorfissam1@gmail.com>
2022-08-03 21:53:24 +0100

    Second commit

--- preprocessing_R.R
+++ preprocessing_R.R
@@ -15,10 +15,22 @@ dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN =
   function(x)  mean(x,na.rm = TRUE)), dataset$Salary)
 print(dataset)
 
-
+# Encode categorical data
 dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1,2,3))
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No','Yes'),
                          labels = c(0,1))
+
+#Splitting the dataset into training dataset and testing dataset
+#install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+testing_set = subset(dataset, split == FALSE)
+
+#Feature scalling 
+training_set[, 2:3] = scale(training_set[, 2:3])
+testing_set[, 2:3] = scale(testing_set[, 2:3])
